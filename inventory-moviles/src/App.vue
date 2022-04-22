499ab375d8f605f3e4e0eb197e2c1708faa9b0c8
ariasdev672 <ariasdev672@gmail.com>
2022-04-22 14:41:16 -0500

    fix some css 💅

---
+++ inventory-moviles/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AppNavbar from './components/AppNavbar.vue'
+</script>
+
+<template>
+  <AppNavbar />
+  <router-view></router-view>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods:{
+    ...mapActions('auth',[
+      'getAuthAll'
+    ]),
+
+    ...mapActions('movement', [
+      'getMovementAll'
+    ])
+  },
+
+  created(){
+    this.getAuthAll()
+    this.getMovementAll()
+  }
+}
+</script>
