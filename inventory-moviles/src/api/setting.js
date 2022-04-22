f814942e41979192666dd1d0b7b9167ff81e0179
ariasdev672 <ariasdev672@gmail.com>
2022-04-22 14:18:30 -0500

    refactor:firebase 🔥 calls as client

---
+++ inventory-moviles/src/api/setting.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBGip9jBMNXfnFNIYunFJd5vLyCnHuzYt0",
+  authDomain: "inventory-moviles.firebaseapp.com",
+  projectId: "inventory-moviles",
+  storageBucket: "inventory-moviles.appspot.com",
+  messagingSenderId: "734699265405",
+  appId: "1:734699265405:web:acf357a7dccd2622fe0014",
+  measurementId: "G-M3WYKB4BBH",
+};
